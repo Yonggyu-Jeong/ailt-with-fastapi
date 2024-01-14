722d99854dfbae9c929b5104606ccab31d203ddb
정용규 <wjddydrbzxzx@naver.com>
2024-01-15 00:18:38 +0900

    build(configs): deleted loggingConfigs in the config and moved it to common/logger/logger.py

--- AiLT/app/common/configs/config.py
+++ AiLT/app/common/configs/config.py
@@ -1,9 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-import logging
 import json
-
-
 
 # JSON 유형의 외부 참조 파일을 읽어오는 코드 config_file로 반환
 with open('./app/config.json', 'r') as config_file:
@@ -56,36 +53,3 @@ class DbConfig:
 
     # BASE_DIR 변수 선언
     BASE_DIR = Path(__file__).resolve().parent.parent
-
-#TODO https://docs.python.org/ko/3/howto/logging.html
-
-@dataclass
-class LoggingConfig:
-    logger = logging.getLogger(__name__)
-
-    # Debug 이상의 로그 메세지 레벨 설정
-    logger.setLevel(logging.DEBUG)
-
-    # 로그의 포맷 설정,
-    formatter = logging.Formatter('%(asctime)s:%(module)s:%(levelname)s:%(message)s',
-                                  '%Y-%m-%d %H:%M:%S')
-
-    # INFO 레벨 이상의 로그를 콘솔에 출력하는 Handler
-    # TODO 핸들러 분리 고민
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-    # DEBUG 레벨 이상의 로그를 `debug.log`에 출력하는 Handler
-    file_debug_handler = logging.FileHandler('debug.log')
-    file_debug_handler.setLevel(logging.DEBUG)
-    file_debug_handler.setFormatter(formatter)
-    logger.addHandler(file_debug_handler)
-
-    # ERROR 레벨 이상의 로그를 `error.log`에 출력하는 Handler
-    file_error_handler = logging.FileHandler('error.log')
-    file_error_handler.setLevel(logging.ERROR)
-    file_error_handler.setFormatter(formatter)
-    logger.addHandler(file_error_handler)
-
