30f37c3f674a9c63889ffcb0607d4a42b3ea8537
정용규 <wjddydrbzxzx@naver.com>
2024-01-15 19:44:34 +0900

    chore(main): delete create_app() and add app.on_event("shutdown")

--- AiLT/app/main.py
+++ AiLT/app/main.py
@@ -2,35 +2,33 @@ from fastapi import FastAPI, Depends
 import uvicorn
 from app.common.configs.config import DbConfig
 from app.database.database import Database
-from app.common.logger.logger import logger
 from app.routers import user_router
+from app.common.logger.logger import logger
 
 """
     create_app()
         return app
 
     fast-api를 구동시, config 파일 안의 
-    미들웨어 정리, 데이터베이스 초기화, 라우터 정의를 차례대로 시작합니다. 
+    미들웨어 정리, 데이터베이스 초기화, 라우터 정의 및 DB 객체를 다른 라우터에 종속성 주입합니다.
 
     작성자 : 정용규
     작성일 : 2024. 01. 14 
 """
-def create_app():
-        app = FastAPI()
-        #TODO 미들웨어 세팅
-        #app.add_middleware()
-        database = Database(DbConfig())
-        app.include_router(user_router.router, prefix="/users", tags=["users"], dependencies=[Depends(database)])
 
-        return app
-
-app = create_app()
+app = FastAPI()
+#TODO 미들웨어 세팅 app.add_middleware()
+database = Database(DbConfig())
+app.include_router(user_router.router, prefix="/users", tags=["users"], dependencies=[Depends(database)])
 
 @app.get("/")
 async def root():
-    logger.info("Info test from main.py")
-    #logger.error("Error test log from main.py")
     return {"message": "Hello World"}
+
+
+@app.on_event("shutdown")
+def shutdown_db():
+    database.close_connection()
 
 # uvicorn 사용하여 FastAPI 실행
 if __name__ == "__main__":
