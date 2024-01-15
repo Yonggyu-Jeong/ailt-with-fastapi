7ce3abe5b46e6e21f5dfdb6cca73c2296b4d2458
정용규 <wjddydrbzxzx@naver.com>
2024-01-15 17:49:38 +0900

    chore(main): create_app() modificatied on main.py and logger test

--- AiLT/app/main.py
+++ AiLT/app/main.py
@@ -1,48 +1,37 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import uvicorn
-
-import app.common.middleware as middleware
-from app.common.configs.config import DbConfig, LoggingConfig
+from app.common.configs.config import DbConfig
 from app.database.database import Database
-import logging
-
-database = 0
-
+from app.common.logger.logger import logger
+from app.routers import user_router
 
 """
     create_app()
         return app
 
     fast-api를 구동시, config 파일 안의 
-    데이터베이스 초기화, 미들웨어 정리, 라우터 정의를 차례대로 시작합니다. 
+    미들웨어 정리, 데이터베이스 초기화, 라우터 정의를 차례대로 시작합니다. 
 
     작성자 : 정용규
     작성일 : 2024. 01. 14 
 """
 def create_app():
         app = FastAPI()
-        db_config = DbConfig()
-        database = Database(db_config)
-        LoggingConfig()
-        #app.add_middleware(middleware)
+        #TODO 미들웨어 세팅
+        #app.add_middleware()
+        database = Database(DbConfig())
+        app.include_router(user_router.router, prefix="/users", tags=["users"], dependencies=[Depends(database)])
 
         return app
 
-
 app = create_app()
-
 
 @app.get("/")
 async def root():
-
+    logger.info("Info test from main.py")
+    #logger.error("Error test log from main.py")
     return {"message": "Hello World"}
-
-
-
-
 
 # uvicorn 사용하여 FastAPI 실행
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-# app.include_router()
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
