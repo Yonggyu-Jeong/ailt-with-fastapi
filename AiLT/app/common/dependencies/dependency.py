12c6d9bf96670cecca8f7401154626954789039f
정용규 <wjddydrbzxzx@naver.com>
2024-01-16 19:41:12 +0900

    feat(dependency): add dependency and removed main`s few init code

---
+++ AiLT/app/common/dependencies/dependency.py
@@ -0,0 +1,35 @@
+from fastapi import Depends
+from app.common.configs.config import DbConfig
+from app.services.user_service import UserService
+from app.database.database import Database
+
+# main.py에 의존성 주입
+
+"""
+    get_database()
+        return Database     
+
+    DbConfig를 이용하여, DataBase를 초기화하고 가져오는 함수입니다.
+
+    작성자 : 정용규
+    작성일 : 2024. 01. 16
+"""
+
+
+def get_database() -> Database:
+    return Database(DbConfig())
+
+
+"""
+    get_user_service(database: Database)
+        return UserService
+
+    Database를 입력 받아, UserService를 초기화하고 가져오는 함수입니다.
+
+    작성자 : 정용규
+    작성일 : 2024. 01. 16
+"""
+
+
+def get_user_service(database: Database = Depends(get_database)) -> UserService:
+    return UserService(database)
--- AiLT/app/main.py
+++ AiLT/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Depends
 import uvicorn
+import app.common.dependencies.dependency as dependency
 from app.common.configs.config import DbConfig
 from app.database.database import Database
 from app.routers import user_router
-from app.common.logger.logger import logger
 
 """
     create_app()
@@ -13,13 +13,18 @@ from app.common.logger.logger import logger
     미들웨어 정리, 데이터베이스 초기화, 라우터 정의 및 DB 객체를 다른 라우터에 종속성 주입합니다.
 
     작성자 : 정용규
+    작성일 : 2024. 01. 15
     작성일 : 2024. 01. 14 
 """
 
 app = FastAPI()
-#TODO 미들웨어 세팅 app.add_middleware()
+database = dependency.get_database()
+# TODO 미들웨어 세팅 app.add_middleware()
+
+app.include_router(user_router.router_user, prefix="/users", tags=["users"],
+                   dependencies=[Depends(dependency.get_user_service(database))])
 database = Database(DbConfig())
-app.include_router(user_router.router, prefix="/users", tags=["users"], dependencies=[Depends(database)])
+
 
 @app.get("/")
 async def root():
@@ -29,6 +34,7 @@ async def root():
 @app.on_event("shutdown")
 def shutdown_db():
     database.close_connection()
+
 
 # uvicorn 사용하여 FastAPI 실행
 if __name__ == "__main__":
