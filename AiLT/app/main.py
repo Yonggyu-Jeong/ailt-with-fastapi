86e5560ff7e5f3acd4af3cc8976eb8549e7b8c72
정용규 <wjddydrbzxzx@naver.com>
2024-01-16 19:37:55 +0900

    feat(dependency): add dependency and removed main`s few init code

--- AiLT/app/main.py
+++ AiLT/app/main.py
@@ -1,25 +1,21 @@
 from fastapi import FastAPI, Depends
 import uvicorn
-from app.common.configs.config import DbConfig
-from app.database.database import Database
+import app.common.dependencies.dependency as dependency
 from app.routers import user_router
-from app.common.logger.logger import logger
 
 """
-    create_app()
-        return app
-
     fast-api를 구동시, config 파일 안의 
     미들웨어 정리, 데이터베이스 초기화, 라우터 정의 및 DB 객체를 다른 라우터에 종속성 주입합니다.
 
     작성자 : 정용규
-    작성일 : 2024. 01. 14 
+    작성일 : 2024. 01. 15
 """
 
 app = FastAPI()
+database = dependency.get_database()
 #TODO 미들웨어 세팅 app.add_middleware()
-database = Database(DbConfig())
-app.include_router(user_router.router, prefix="/users", tags=["users"], dependencies=[Depends(database)])
+
+app.include_router(user_router.router_user, prefix="/users", tags=["users"], dependencies=[Depends(dependency.get_user_service(database))])
 
 @app.get("/")
 async def root():
