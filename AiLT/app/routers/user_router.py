87ae2e84468bcd8bfff3831e3ac1039387ce247a
정용규 <wjddydrbzxzx@naver.com>
2024-01-16 22:42:19 +0900

    chore(user_router): add httpexception

--- AiLT/app/routers/user_router.py
+++ AiLT/app/routers/user_router.py
@@ -1,57 +1,75 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.models.user_model import UserDto
-from app.common.logger.logger import logger
 from app.common.dependencies.dependency import get_test_user_service as get_user_service
 from tests.user_test.test_user_service import TestUserService as UserService
 
 # from app.common.dependencies.dependency import get_user_service
 # from app.services.user_service import UserService
-
 router_user = APIRouter()
 
 
-# TODO depend()를 이용한 라우터 전역 의존성 함수 테스팅. TDD 방식으로 코드 테스트
-# TODO TDD 방법론 다시 공부, 주석 처리 방법
-
 @router_user.get("/")
 def read_users(user_service: UserService = Depends(get_user_service)):
-    users = user_service.get_users()
-    return users
+    try:
+        users = user_service.get_users()
+        return users
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 
 @router_user.get("/{user_id}")
 def read_user(user_id: int,
               user_service: UserService = Depends(get_user_service)):
-    user = user_service.get_user(user_id)
-    if user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return user
+    try:
+        user = user_service.get_user(user_id)
+        if user is None:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 
 @router_user.post("/users/")
 def create_user(user_data: UserDto,
                 user_service: UserService = Depends(get_user_service)):
-    user_service.create_user(user_data)
-    return {"message": "User created successfully"}
+    try:
+        user_service.create_user(user_data)
+        return {"message": "create_user successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+
+@router_user.put("/{user_id}")
+def update_user(user_id: int, user_data: UserDto,
+                user_service: UserService = Depends(get_user_service)):
+    try:
+        user_service.put_user(user_id, user_data)
+        return {"message": f"put_user successfully: {user_id}"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 
 @router_user.patch("/{user_id}")
-def update_user(user_id: int,
-                user_data: UserDto,
+def update_user(user_id: int, user_data: UserDto,
                 user_service: UserService = Depends(get_user_service)):
-    user_service.update_user(user_id, user_data)
-    return {"message": f"User with ID {user_id} updated successfully"}
+    try:
+        user_service.patch_user(user_id, user_data)
+        return {"message": f"patch_user successfully: {user_id}"}
 
-
-def put_user(user_id: int,
-             user_data: UserDto,
-             user_service: UserService = Depends(get_user_service)
-):
-    user_service.update_user(user_id, user_data)
-    return {"message": f"User with ID {user_id} updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
 
 
 @router_user.delete("/{user_id}")
-def delete_user(user_id: int, user_service: UserService = Depends(get_user_service)):
-    user_service.delete_user(user_id)
-    return {"message": f"User with ID {user_id} deleted successfully"}
+def delete_user(user_id: int,
+                user_service: UserService = Depends(get_user_service)):
+    try:
+        user_service.delete_user(user_id)
+        return {"message": f"delete_user successfully: {user_id}"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
