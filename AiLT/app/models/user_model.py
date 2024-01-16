9c4a0a6d0e311cfdc666babce6d49e893d6abe16
정용규 <wjddydrbzxzx@naver.com>
2024-01-16 19:41:56 +0900

    feat(model): Added model.py and modified user_model to inherit relationship.

--- AiLT/app/models/user_model.py
+++ AiLT/app/models/user_model.py
@@ -1,4 +1,15 @@
-from pydantic import BaseModel
+from app.models.model import ModelDto
 
-class UserDto(BaseModel):
-    user: str
+"""
+    class UserDto(ModelDto)
+        
+    부모 클래스인 model의 ModelDto를 상속받은 모델입니다.     
+
+    작성자 : 정용규
+    작성일 : 2024. 01. 16   
+"""
+
+
+class UserDto(ModelDto):
+    username: str
+    email: str
