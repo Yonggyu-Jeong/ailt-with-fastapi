7a8cd9c506ea202eb909f7bd7f35ac8042f76841
정용규 <wjddydrbzxzx@naver.com>
2024-02-24 20:55:56 +0900

    build(env): commit pycache and removed some modulse

--- AiLT/test_torch.py
+++
@@ -1,9 +0,0 @@
-import torch, gc
-
-try:
-    gc.collect()
-    torch.cuda.empty_cache()
-    print("============================is_done============================")
-
-except RuntimeError as e:
-    print("============================RuntimeError============================")
